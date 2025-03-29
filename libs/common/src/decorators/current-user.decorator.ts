6240c1e8dee0100759b1d87ffedeae29bfeeeb5a
zulfikar-ditya <zulfikarditya.work@gmail.com>
2025-03-29 07:16:52 +0700

    fix: remove unused parameter assignment in CurrentUser decorator

--- libs/common/src/decorators/current-user.decorator.ts
+++ libs/common/src/decorators/current-user.decorator.ts
@@ -4,6 +4,7 @@ import { Request } from 'express';
 export const CurrentUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request: Request = ctx.switchToHttp().getRequest();
+    data = data;
     return request.user;
   },
 );
